8b81f0603f98f4a6dfca6c9924efd1be449f9be9
Dave Trueman <d.trueman@gmail.com>
2009-12-03 16:20:14 -0500

    Added a use_ssl option for use with S3

--- lib/backup/configuration/storage.rb
+++ lib/backup/configuration/storage.rb
@@ -3,7 +3,7 @@ module Backup
     class Storage
       attr_accessor :attributes 
 
-      %w(ip user password path access_key_id secret_access_key bucket).each do |method|
+      %w(ip user password path access_key_id secret_access_key use_ssl bucket).each do |method|
         define_method method do |value|
           attributes[method] = value
         end
