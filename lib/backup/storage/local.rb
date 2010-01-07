56c7ccf27541c7fe2ada462df5c79d26c93b48dc
Fernando Luizao <fernandoluizao@gmail.com>
2010-01-07 14:55:17 -0200

    using CommandRunner in Local storage, and copy the backup instead of move

--- lib/backup/storage/local.rb
+++ lib/backup/storage/local.rb
@@ -2,6 +2,8 @@ module Backup
   module Storage
     class Local
       
+      include Backup::CommandHelper
+      
       # Store on same machine, preferentially in a different hard drive or in 
       # a mounted network path (NFS, Samba, etc)
       attr_accessor :path, :tmp_path, :final_file
@@ -12,8 +14,8 @@ module Backup
         self.tmp_path   = adapter.tmp_path
         self.final_file = adapter.final_file
         
-        %x{ mkdir -p #{path} }
-        %x{ mv #{File.join(tmp_path, final_file).gsub('\ ', ' ')} #{File.join(path, final_file)} }
+        run "mkdir -p #{path}"
+        run "cp #{File.join(tmp_path, final_file).gsub('\ ', ' ')} #{File.join(path, final_file)}"
       end
       
     end
