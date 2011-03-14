cc67156e3c061389d67d2600c6075fd8d92940a5
Michael van Rooijen <meskyanichi@gmail.com>
2011-03-14 01:23:20 +0100

    Expanded a bit of the Archive specs.

--- spec/archive_spec.rb
+++ spec/archive_spec.rb
@@ -61,11 +61,25 @@ describe Backup::Archive do
       Backup::Logger.stubs(:message)
     end
 
-    it 'should tar all the specified paths' do
-      archive.expects(:mkdir).with(File.join(Backup::TMP_PATH, Backup::TRIGGER, 'archive'))
-      archive.expects(:run).with("tar -c --exclude={'/home/rspecuser/excludefile','/home/rspecuser/excludedir/'} '/home/rspecuser/somefile' '/home/rspecuser/logs/' '/home/rspecuser/dotfiles/' 1> '#{File.join(Backup::TMP_PATH, Backup::TRIGGER, 'archive', "#{:dummy_archive}.tar")}' 2> /dev/null")
-      archive.expects(:utility).with(:tar).returns(:tar)
-      archive.perform!
+    context 'when both paths were added and paths that should be excluded were added' do
+      it 'should render both the syntax for the paths that be included as well as excluded' do
+        archive.expects(:mkdir).with(File.join(Backup::TMP_PATH, Backup::TRIGGER, 'archive'))
+        archive.expects(:run).with("tar -c --exclude={'/home/rspecuser/excludefile','/home/rspecuser/excludedir/'} '/home/rspecuser/somefile' '/home/rspecuser/logs/' '/home/rspecuser/dotfiles/' 1> '#{File.join(Backup::TMP_PATH, Backup::TRIGGER, 'archive', "#{:dummy_archive}.tar")}' 2> /dev/null")
+        archive.expects(:utility).with(:tar).returns(:tar)
+        archive.perform!
+      end
+    end
+
+    context 'when there are paths to add, and no exclude patterns were defined' do
+      it 'should only render syntax for the defined paths' do
+        archive = Backup::Archive.new(:dummy_archive) do |a|
+          a.add '/path/to/archive'
+        end
+
+        archive.stubs(:utility).returns(:tar)
+        archive.expects(:run).with("tar -c  '/path/to/archive' 1> '#{File.join(Backup::TMP_PATH, Backup::TRIGGER, 'archive', "#{:dummy_archive}.tar")}' 2> /dev/null")
+        archive.perform!
+      end
     end
 
     it 'should log the status' do
