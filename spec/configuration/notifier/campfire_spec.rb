4f7501dd6c1c3d58406e5c47898bcd4b31601e78
Michael van Rooijen <meskyanichi@gmail.com>
2011-03-29 18:38:42 +0200

    Added the Campfire and Twitter configuration specs.

---
+++ spec/configuration/notifier/campfire_spec.rb
@@ -0,0 +1,20 @@
+# encoding: utf-8
+
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe Backup::Configuration::Notifier::Campfire do
+  before do
+    Backup::Configuration::Notifier::Campfire.defaults do |campfire|
+      campfire.token     = 'my_api_authentication_token'
+      campfire.subdomain = 'my_subdomain'
+      campfire.room_id   = 'my_room_id'
+    end
+  end
+
+  it 'should set the default campfire configuration' do
+    campfire = Backup::Configuration::Notifier::Campfire
+    campfire.token.should     == 'my_api_authentication_token'
+    campfire.subdomain.should == 'my_subdomain'
+    campfire.room_id.should   == 'my_room_id'
+  end
+end
