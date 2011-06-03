c82f17feeb829217e2ecba9f3ea643aef16c3d98
Michal Cichra <m@o2h.cz>
2011-06-03 22:43:52 +0200

    Added support for trigger wildcards! Implements #136

--- lib/backup/finder.rb
+++ lib/backup/finder.rb
@@ -3,26 +3,25 @@
 module Backup
   class Finder
     attr_accessor :trigger, :config
+    
+    ##
+    # The wildcard character to match triggers
+    # Can be used alone or in mask (e.g. web_* )
+    WILDCARD = '*' 
 
     ##
     # Initializes a new Backup::Finder object
     # and stores the path to the configuration file
-    def initialize(trigger, config)
+    def initialize(trigger, config = CONFIG_FILE)
       @trigger = trigger.to_sym
       @config  = config
     end
 
     ##
-    # Tries to find and load the configuration file and return the proper
+    # Tries to find and return the proper
     # backup model configuration (specified by the 'trigger')
     def find
-      unless File.exist?(config)
-        puts "Could not find a configuration file in '#{config}'."; exit
-      end
-
-      ##
-      # Loads the backup configuration file
-      instance_eval(File.read(config))
+      load_config!
 
       ##
       # Iterates through all the instantiated backup models and returns
@@ -34,6 +33,47 @@ module Backup
       end
 
       puts "Could not find trigger '#{trigger}' in '#{config}'."; exit
+    end
+    
+    ##
+    # Tries to find and return the all triggers
+    # matching wildcard (specified by the 'trigger')
+    def matching
+      ##
+      # Define the TIME constants unless defined
+      ::Backup.send(:const_set, :TIME, Time.now.strftime("%Y.%m.%d.%H.%M.%S")) unless defined? Backup::TIME
+      
+      ##
+      # Parses the backup configuration file
+      load_config!
+      
+      triggers = Backup::Model.all.map{|model| model.trigger.to_s }
+      
+      ##
+      # Removes the TIME constant
+      ::Backup.send(:remove_const, :TIME) if defined? Backup::TIME
+      
+      ##
+      # Make regexp replacing wildcard character by (.+)
+      wildcard = %r{^#{trigger.to_s.gsub(WILDCARD, '(.+)')}$}
+      
+      ##
+      # Returns all trigger names matching wildcard
+      triggers.select { |trigger| trigger =~ wildcard }
+    end
+    
+    private
+    
+    ##
+    # Tries to find and load the configuration file
+    def load_config!
+      unless File.exist?(config)
+        puts "Could not find a configuration file in '#{config}'."; exit
+      end
+
+      ##
+      # Loads the backup configuration file
+      instance_eval(File.read(config))
     end
   end
 end
