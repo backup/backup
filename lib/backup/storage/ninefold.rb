d64aeeb30ccfd00535b8135f45bf29697be2b601
Pat Allan <pat@freelancing-gods.com>
2011-08-01 12:56:59 +1000

    Adding first pass at Ninefold support. Haven't got specs running yet, mind you - but lifted S3 code, adapted as necessary.

---
+++ lib/backup/storage/ninefold.rb
@@ -0,0 +1,96 @@
+# encoding: utf-8
+
+##
+# Only load the Fog gem when the Backup::Storage::Ninefold class is loaded
+Backup::Dependency.load('fog')
+
+module Backup
+  module Storage
+    class Ninefold < Base
+
+      ##
+      # Ninefold Credentials
+      attr_accessor :storage_token, :storage_secret
+
+      ##
+      # Ninefold directory path
+      attr_accessor :path
+
+      ##
+      # Creates a new instance of the Ninefold storage object
+      # First it sets the defaults (if any exist) and then evaluates
+      # the configuration block which may overwrite these defaults
+      #
+      def initialize(&block)
+        load_defaults!
+
+        @path ||= 'backups'
+
+        instance_eval(&block) if block_given?
+
+        @time = TIME
+      end
+
+      ##
+      # This is the remote path to where the backup files will be stored
+      def remote_path
+        File.join(path, TRIGGER).sub(/^\//, '')
+      end
+
+      ##
+      # This is the provider that Fog uses for the Ninefold storage
+      def provider
+        'Ninefold'
+      end
+
+      ##
+      # Performs the backup transfer
+      def perform!
+        transfer!
+        cycle!
+      end
+
+    private
+
+      ##
+      # Establishes a connection to Amazon S3 and returns the Fog object.
+      # Not doing any instance variable caching because this object gets persisted in YAML
+      # format to a file and will issues. This, however has no impact on performance since it only
+      # gets invoked once per object for a #transfer! and once for a remove! Backups run in the
+      # background anyway so even if it were a bit slower it shouldn't matter.
+      def connection
+        Fog::Storage.new(
+          :provider                => provider,
+          :ninefold_storage_token  => storage_token,
+          :ninefold_storage_secret => storage_secret
+        )
+      end
+
+      ##
+      # Transfers the archived file to the specified Amazon S3 bucket
+      def transfer!
+        begin
+          Logger.message("#{ self.class } started transferring \"#{ remote_file }\".")
+          connection.sync_clock
+          connection.put_object(
+            bucket,
+            File.join(remote_path, remote_file),
+            File.open(File.join(local_path, local_file))
+          )
+        rescue Excon::Errors::NotFound
+          raise "An error occurred while trying to transfer the backup, please make sure the bucket exists."
+        end
+      end
+
+      ##
+      # Removes the transferred archive file from the Amazon S3 bucket
+      def remove!
+        begin
+          connection.sync_clock
+          connection.delete_object(bucket, File.join(remote_path, remote_file))
+        rescue Excon::Errors::SocketError; end
+      end
+
+    end
+  end
+end
