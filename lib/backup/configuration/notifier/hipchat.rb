97bc74798c839d18319b24491c9bdb8babc3d66e
Jesse Dearing <jesse.dearing@gmail.com>
2011-11-21 14:39:52 -0600

    Updating rdoc in configuration class

--- lib/backup/configuration/notifier/hipchat.rb
+++ lib/backup/configuration/notifier/hipchat.rb
@@ -18,13 +18,16 @@ module Backup
           # Rooms that you want to post notifications to
           attr_accessor :rooms_notified
 
+          # Success Color
           # The background color of a success message. One of :yellow, :red, :green, :purple, or :random. (default: yellow)
           attr_accessor :success_color
 
+          # Failure Color
           # The background color of an error message. One of :yellow, :red, :green, :purple, or :random. (default: yellow)
           attr_accessor :failure_color
 
-          # Notify users in the room
+          # Notify Users
+          # (bool) Notify users in the room
           attr_accessor :notify_users
         end
       end
