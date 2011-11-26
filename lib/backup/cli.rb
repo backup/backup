8ef09a74cee6ee064a656215a149e8eff4214a1b
Michael van Rooijen <meskyanichi@gmail.com>
2011-11-26 17:02:13 +0100

    The Backup::CLI module now returns the STDOUT when no exception has been raised due to non-whitelisted status codes.

--- lib/backup/cli.rb
+++ lib/backup/cli.rb
@@ -28,6 +28,7 @@ module Backup
       process_data[:ignore_exit_codes] = ((options[:ignore_exit_codes] || Array.new) << 0).uniq
 
       raise_if_command_failed!(command_name(command), process_data)
+      process_data[:stdout]
     end
 
     ##
