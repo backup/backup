d2a58c33269fc0d32eaf9fd25d699598b7e3b814
Michael van Rooijen <meskyanichi@gmail.com>
2011-11-29 22:04:49 +0100

    Added Riak configuration specs.

---
+++ spec/configuration/database/riak_spec.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe Backup::Configuration::Database::Riak do
+  before do
+    Backup::Configuration::Database::Riak.defaults do |db|
+      db.name   = 'mydb'
+      db.node   = '/var/lib/redis/db'
+      db.cookie = 'mypassword'
+    end
+  end
+
+  it 'should set the default Riak configuration' do
+    db = Backup::Configuration::Database::Riak
+    db.name.should   == 'mydb'
+    db.node.should   == '/var/lib/redis/db'
+    db.cookie.should == 'mypassword'
+  end
+
+  describe '#clear_defaults!' do
+    it 'should clear all the defaults, resetting them to nil' do
+      Backup::Configuration::Database::Riak.clear_defaults!
+
+      db = Backup::Configuration::Database::Riak
+      db.name.should   == nil
+      db.node.should   == nil
+      db.cookie.should == nil
+    end
+  end
+end
