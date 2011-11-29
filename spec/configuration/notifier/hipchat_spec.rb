153d0c7b61ad3f224e49531a66fc6ec835b78f4f
Michael van Rooijen <meskyanichi@gmail.com>
2011-11-29 22:10:15 +0100

    Added configuration specs for Hipchat.

---
+++ spec/configuration/notifier/hipchat_spec.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe Backup::Configuration::Notifier::Hipchat do
+  before do
+    Backup::Configuration::Notifier::Hipchat.defaults do |hipchat|
+      hipchat.token          = 'token'
+      hipchat.from           = 'DB Backup'
+      hipchat.rooms_notified = ['activity']
+      hipchat.success_color  = 'green'
+      hipchat.failure_color  = 'red'
+    end
+  end
+
+  it 'should set the default tweet configuration' do
+    hipchat = Backup::Configuration::Notifier::Hipchat
+    hipchat.token.should          == 'token'
+    hipchat.from.should           == 'DB Backup'
+    hipchat.rooms_notified.should == ['activity']
+    hipchat.success_color.should  == 'green'
+    hipchat.failure_color.should  == 'red'
+  end
+
+  describe '#clear_defaults!' do
+    it 'should clear all the defaults, resetting them to nil' do
+      Backup::Configuration::Notifier::Hipchat.clear_defaults!
+
+      hipchat = Backup::Configuration::Notifier::Hipchat
+      hipchat.token.should          == nil
+      hipchat.from.should           == nil
+      hipchat.rooms_notified.should == nil
+      hipchat.success_color.should  == nil
+      hipchat.failure_color.should  == nil
+    end
+  end
+end
