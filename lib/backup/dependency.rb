c3fd8e6eb4f464de1c87cbad969651d2ef05ba9d
Max Riveiro <kavu13@gmail.com>
2012-12-02 16:39:48 +0400

    Bump Fog dependency up to 1.9.0

--- lib/backup/dependency.rb
+++ lib/backup/dependency.rb
@@ -17,7 +17,7 @@ module Backup
       {
         'fog' => {
           :require => 'fog',
-          :version => '~> 1.4.0',
+          :version => '~> 1.9.0',
           :for     => 'Amazon S3, Rackspace Cloud Files (S3, CloudFiles Storages)'
         },
 
