9746f8a39989ac57d31b17a0683d178769f153e8
Brian D. Burns <burns@iosctr.com>
2013-03-13 15:10:05 -0400

    Release v3.1.3

--- lib/backup/version.rb
+++ lib/backup/version.rb
@@ -13,7 +13,7 @@ module Backup
     #  Defines the minor version
     # PATCH:
     #  Defines the patch version
-    MAJOR, MINOR, PATCH = 3, 1, 2
+    MAJOR, MINOR, PATCH = 3, 1, 3
 
     ##
     # Returns the major version ( big release based off of multiple minor releases )
