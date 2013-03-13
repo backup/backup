08e5b97b851e20c618162ecc810bc3991e5b1787
Jason Lee <huacnlee@gmail.com>
2013-03-13 11:06:47 +0800

    Fix cli test.

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -467,7 +467,7 @@ describe 'Backup::CLI' do
         -t, --trigger=TRIGGER
             [--config-path=CONFIG_PATH]  # Path to your Backup configuration directory
             [--databases=DATABASES]      # (mongodb, mysql, postgresql, redis, riak)
-            [--storages=STORAGES]        # (cloud_files, dropbox, ftp, local, ninefold, rsync, s3, scp, sftp)
+            [--storages=STORAGES]        # (aliyun, cloud_files, dropbox, ftp, local, ninefold, rsync, s3, scp, sftp)
             [--syncers=SYNCERS]          # (cloud_files, rsync_local, rsync_pull, rsync_push, s3)
             [--encryptors=ENCRYPTORS]    # (gpg, openssl)
             [--compressors=COMPRESSORS]  # (bzip2, custom, gzip, lzma, pbzip2)
