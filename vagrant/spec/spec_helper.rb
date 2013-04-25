2ccb3f8d3f86a8760311390c2acbae68f912de1c
Brian D. Burns <iosctr@gmail.com>
2013-04-25 16:47:11 -0400

    VM version 5

--- vagrant/spec/spec_helper.rb
+++ vagrant/spec/spec_helper.rb
@@ -3,7 +3,7 @@
 abort "These specs should only be run on the backup-testbox VM" unless
     %x[hostname].chomp == 'backup-testbox'
 
-version = '4'
+version = '5'
 found = File.read('/home/vagrant/backup-testbox-version').strip rescue '?'
 warn(<<EOS) unless version == found
 \n  -- Warning: backup-testbox should be v.#{ version } - Found v.#{ found } --
