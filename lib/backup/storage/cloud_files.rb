aa3a467fa06f2793f0c61cfc21befdf0a263b3d2
Brian D. Burns <iosctr@gmail.com>
2013-07-20 08:43:34 -0400

    update Storage::CloudFiles to use CloudIO

--- lib/backup/storage/cloud_files.rb
+++ lib/backup/storage/cloud_files.rb
@@ -1,13 +1,18 @@
 # encoding: utf-8
-require 'fog'
+require 'backup/cloud_io/cloud_files'
 
 module Backup
   module Storage
     class CloudFiles < Base
+      Error = Errors::Storage::CloudFiles::Error
 
       ##
-      # Rackspace Cloud Files Credentials
-      attr_accessor :username, :api_key, :auth_url
+      # Rackspace CloudFiles Credentials
+      attr_accessor :username, :api_key
+
+      ##
+      # Rackspace Auth URL (optional)
+      attr_accessor :auth_url
 
       ##
       # Rackspace Service Net
@@ -15,40 +20,92 @@ module Backup
       attr_accessor :servicenet
 
       ##
-      # Rackspace Cloud Files container name
+      # Rackspace Region (optional)
+      attr_accessor :region
+
+      ##
+      # Rackspace Container Name
       attr_accessor :container
+
+      ##
+      # Rackspace Container Name for SLO Segments
+      # Required if #segment_size is set. Must be different from #container.
+      attr_accessor :segments_container
+
+      ##
+      # SLO Segment size, specified in MiB.
+      #
+      # Each package file larger than +segment_size+
+      # will be uploaded as a Static Large Objects (SLO).
+      #
+      # Defaults to 0 for backward compatibility (pre v.3.7.0),
+      # since #segments_container would be required.
+      #
+      # Minimum: 1 (0 disables SLO support)
+      # Maximum: 5120 (5 GiB)
+      attr_accessor :segment_size
+
+      ##
+      # If set, all backup package files (including SLO segments) will be
+      # scheduled for automatic removal by the server.
+      #
+      # The `keep` option should not be used if this is set,
+      # unless you're transitioning from the `keep` option.
+      attr_accessor :days_to_keep
+
+      ##
+      # Number of times to retry failed operations.
+      #
+      # Default: 10
+      attr_accessor :max_retries
+
+      ##
+      # Time in seconds to pause before each retry.
+      #
+      # Default: 30
+      attr_accessor :retry_waitsec
 
       def initialize(model, storage_id = nil)
         super
 
-        @servicenet ||= false
-        @path       ||= 'backups'
+        @servicenet         ||= false
+        @segment_size       ||= 0
+        @max_retries        ||= 10
+        @retry_waitsec      ||= 30
+
+        @path ||= 'backups'
         path.sub!(/^\//, '')
+
+        check_configuration
       end
 
       private
 
-      def connection
-        @connection ||= Fog::Storage.new(
-          :provider             => 'Rackspace',
-          :rackspace_username   => username,
-          :rackspace_api_key    => api_key,
-          :rackspace_auth_url   => auth_url,
-          :rackspace_servicenet => servicenet
+      def cloud_io
+        @cloud_io ||= CloudIO::CloudFiles.new(
+          :username           => username,
+          :api_key            => api_key,
+          :auth_url           => auth_url,
+          :region             => region,
+          :servicenet         => servicenet,
+          :container          => container,
+          :segments_container => segments_container,
+          :segment_size       => segment_size,
+          :days_to_keep       => days_to_keep,
+          :max_retries        => max_retries,
+          :retry_waitsec      => retry_waitsec
         )
       end
 
       def transfer!
-        connection.put_container(container)
-
         package.filenames.each do |filename|
           src = File.join(Config.tmp_path, filename)
           dest = File.join(remote_path, filename)
           Logger.info "Storing '#{ container }/#{ dest }'..."
-          File.open(src, 'r') do |file|
-            connection.put_object(container, dest, file)
-          end
+          cloud_io.upload(src, dest)
         end
+
+        package.no_cycle = true if days_to_keep
       end
 
       # Called by the Cycler.
@@ -57,9 +114,36 @@ module Backup
         Logger.info "Removing backup package dated #{ package.time }..."
 
         remote_path = remote_path_for(package)
-        package.filenames.each do |filename|
-          connection.delete_object(container, File.join(remote_path, filename))
-        end
+        objects = cloud_io.objects(remote_path)
+
+        raise Error, "Package at '#{ remote_path }' not found" if objects.empty?
+
+        slo_objects, objects = objects.partition(&:slo?)
+        cloud_io.delete_slo(slo_objects)
+        cloud_io.delete(objects)
+      end
+
+      def check_configuration
+        required = %w{ username api_key container }
+        raise Error, <<-EOS if required.map {|name| send(name) }.any?(&:nil?)
+          Configuration Error
+          #{ required.map {|name| "##{ name }"}.join(', ') } are all required
+        EOS
+
+        raise Error, <<-EOS if segment_size > 0 && segments_container.to_s.empty?
+          Configuration Error
+          #segments_container is required if #segment_size is > 0
+        EOS
+
+        raise Error, <<-EOS if container == segments_container
+          Configuration Error
+          #container and #segments_container must not be the same container.
+        EOS
+
+        raise Error, <<-EOS if segment_size > 5120
+          Configuration Error
+          #segment_size is too large (max 5120)
+        EOS
       end
 
     end
--- vagrant/spec/live/storage/cloud_files_spec.rb
+++ vagrant/spec/live/storage/cloud_files_spec.rb
@@ -5,117 +5,208 @@ require File.expand_path('../../../spec_helper', __FILE__)
 # To run these tests, you need to setup your Cloudfiles credentials in
 #   /vagrant/spec/live.yml
 #
-# It's recommended you use a dedicated Container for this, like:
+# It's recommended you use dedicated Containers for this, like:
 #   backup.testing.container
+#   backup.testing.segments.container
 #
 module Backup
 describe Storage::CloudFiles,
     :if => BackupSpec::LIVE['storage']['cloudfiles']['specs_enabled'] == true do
 
-  it 'stores package file', :live do
-    create_model :my_backup, <<-EOS
+  before { clean_remote }
+  after  { clean_remote }
+
+  # Each archive is 1.09 MB (1,090,000).
+  # Each job here will create 2 package files (6,291,456 + 248,544).
+  # With :segment_size at 2 MiB, the first package file will be stored
+  # as a SLO with 3 segments. The second package file will use put_object.
+
+  it 'stores package', :live do
+    create_model :my_backup, %q{
       Backup::Model.new(:my_backup, 'a description') do
-        archive :archive_a do |archive|
-          archive.add '~/test_data/dir_a/file_a'
+        split_into_chunks_of 6 # MiB
+
+        6.times do |n|
+          archive "archive_#{ n }" do |archive|
+            archive.add '~/test_data'
+          end
         end
 
+        config = BackupSpec::LIVE['storage']['cloudfiles']
         store_with CloudFiles do |cf|
-          cf.username   = BackupSpec::LIVE['storage']['cloudfiles']['username']
-          cf.api_key    = BackupSpec::LIVE['storage']['cloudfiles']['api_key']
-          cf.auth_url   = BackupSpec::LIVE['storage']['cloudfiles']['auth_url']
-          cf.servicenet = BackupSpec::LIVE['storage']['cloudfiles']['servicenet']
-          cf.container  = BackupSpec::LIVE['storage']['cloudfiles']['container']
-          cf.path       = BackupSpec::LIVE['storage']['cloudfiles']['path']
+          cf.username           = config['username']
+          cf.api_key            = config['api_key']
+          cf.auth_url           = config['auth_url']
+          cf.region             = config['region']
+          cf.servicenet         = config['servicenet']
+          cf.container          = config['container']
+          cf.segments_container = config['segments_container']
+          cf.path               = config['path']
+          cf.max_retries        = 3
+          cf.retry_waitsec      = 5
+          cf.segment_size       = 2 # MiB
+          cf.days_to_keep = 1
         end
       end
-    EOS
+    }
 
     job = backup_perform :my_backup
-    package = BackupSpec::CloudFilesPackage.new(job.model)
 
-    expect( package.files_sent.count ).to be(1)
-    expect( package.files_on_remote ).to eq package.files_sent
+    files_sent = files_sent_for(job)
+    expect( files_sent.count ).to be(2)
 
-    package.clean_remote!
+    objects_on_remote = objects_on_remote_for(job)
+    expect( objects_on_remote.map(&:name) ).to eq files_sent
+    expect( objects_on_remote.all?(&:marked_for_deletion?) ).to be(true)
+
+    segments_on_remote = segments_on_remote_for(job)
+    expect( segments_on_remote.count ).to be(3)
+    expect( segments_on_remote.all?(&:marked_for_deletion?) ).to be(true)
   end
 
-  # With Splitter set a 1 MiB, this will create 3 package files.
-  it 'stores multiple package files', :live do
-    create_model :my_backup, <<-EOS
+  it 'cycles package', :live do
+    create_model :my_backup, %q{
       Backup::Model.new(:my_backup, 'a description') do
-        split_into_chunks_of 1
+        split_into_chunks_of 6 # MiB
 
-        archive :archive_a do |archive|
-          archive.add '~/test_data'
-        end
-        archive :archive_b do |archive|
-          archive.add '~/test_data'
+        6.times do |n|
+          archive "archive_#{ n }" do |archive|
+            archive.add '~/test_data'
+          end
         end
 
+        config = BackupSpec::LIVE['storage']['cloudfiles']
         store_with CloudFiles do |cf|
-          cf.username   = BackupSpec::LIVE['storage']['cloudfiles']['username']
-          cf.api_key    = BackupSpec::LIVE['storage']['cloudfiles']['api_key']
-          cf.auth_url   = BackupSpec::LIVE['storage']['cloudfiles']['auth_url']
-          cf.servicenet = BackupSpec::LIVE['storage']['cloudfiles']['servicenet']
-          cf.container  = BackupSpec::LIVE['storage']['cloudfiles']['container']
-          cf.path       = BackupSpec::LIVE['storage']['cloudfiles']['path']
-        end
-      end
-    EOS
-
-    job = backup_perform :my_backup
-    package = BackupSpec::CloudFilesPackage.new(job.model)
-
-    expect( package.files_sent.count ).to be(3)
-    expect( package.files_on_remote ).to eq package.files_sent
-
-    package.clean_remote!
-  end
-
-  it 'cycles stored packages', :live do
-    create_model :my_backup, <<-EOS
-      Backup::Model.new(:my_backup, 'a description') do
-        archive :archive_a do |archive|
-          archive.add '~/test_data/dir_a/file_a'
-        end
-
-        store_with CloudFiles do |cf|
-          cf.username   = BackupSpec::LIVE['storage']['cloudfiles']['username']
-          cf.api_key    = BackupSpec::LIVE['storage']['cloudfiles']['api_key']
-          cf.auth_url   = BackupSpec::LIVE['storage']['cloudfiles']['auth_url']
-          cf.servicenet = BackupSpec::LIVE['storage']['cloudfiles']['servicenet']
-          cf.container  = BackupSpec::LIVE['storage']['cloudfiles']['container']
-          cf.path       = BackupSpec::LIVE['storage']['cloudfiles']['path']
+          cf.username           = config['username']
+          cf.api_key            = config['api_key']
+          cf.auth_url           = config['auth_url']
+          cf.region             = config['region']
+          cf.servicenet         = config['servicenet']
+          cf.container          = config['container']
+          cf.segments_container = config['segments_container']
+          cf.path               = config['path']
+          cf.max_retries        = 3
+          cf.retry_waitsec      = 5
+          cf.segment_size       = 2 # MiB
           cf.keep = 2
         end
       end
-    EOS
+    }
 
-    job = backup_perform :my_backup
-    package_a = BackupSpec::CloudFilesPackage.new(job.model)
+    job_a = backup_perform :my_backup
+    job_b = backup_perform :my_backup
 
-    job = backup_perform :my_backup
-    package_b = BackupSpec::CloudFilesPackage.new(job.model)
+    # package files for job_a should be on the remote
+    files_sent = files_sent_for(job_a)
+    expect( files_sent.count ).to be(2)
 
-    # a and b should be on the remote
-    expect( package_a.files_sent.count ).to be(1)
-    expect( package_a.files_on_remote ).to eq package_a.files_sent
-    expect( package_b.files_sent.count ).to be(1)
-    expect( package_b.files_on_remote ).to eq package_b.files_sent
+    objects_on_remote = objects_on_remote_for(job_a)
+    expect( objects_on_remote.map(&:name) ).to eq files_sent
+    expect( objects_on_remote.any?(&:marked_for_deletion?) ).to be(false)
 
-    job = backup_perform :my_backup
-    package_c = BackupSpec::CloudFilesPackage.new(job.model)
+    segments_on_remote = segments_on_remote_for(job_a)
+    expect( segments_on_remote.count ).to be(3)
+    expect( segments_on_remote.any?(&:marked_for_deletion?) ).to be(false)
 
-    # b and c should be on the remote
-    expect( package_b.files_sent.count ).to be(1)
-    expect( package_b.files_on_remote ).to eq package_b.files_sent
-    expect( package_c.files_sent.count ).to be(1)
-    expect( package_c.files_on_remote ).to eq package_c.files_sent
+    # package files for job_b should be on the remote
+    files_sent = files_sent_for(job_b)
+    expect( files_sent.count ).to be(2)
 
-    # a should be gone
-    expect( package_a.files_on_remote ).to be_empty
+    objects_on_remote = objects_on_remote_for(job_b)
+    expect( objects_on_remote.map(&:name) ).to eq files_sent
+    expect( objects_on_remote.any?(&:marked_for_deletion?) ).to be(false)
 
-    package_c.clean_remote!
+    segments_on_remote = segments_on_remote_for(job_b)
+    expect( segments_on_remote.count ).to be(3)
+    expect( segments_on_remote.any?(&:marked_for_deletion?) ).to be(false)
+
+    job_c = backup_perform :my_backup
+
+    # package files for job_b should still be on the remote
+    files_sent = files_sent_for(job_b)
+    expect( files_sent.count ).to be(2)
+    expect( objects_on_remote_for(job_b).map(&:name) ).to eq files_sent
+    expect( segments_on_remote_for(job_b).count ).to be(3)
+
+    # package files for job_c should be on the remote
+    files_sent = files_sent_for(job_c)
+    expect( files_sent.count ).to be(2)
+
+    objects_on_remote = objects_on_remote_for(job_c)
+    expect( objects_on_remote.map(&:name) ).to eq files_sent
+    expect( objects_on_remote.any?(&:marked_for_deletion?) ).to be(false)
+
+    segments_on_remote = segments_on_remote_for(job_c)
+    expect( segments_on_remote.count ).to be(3)
+    expect( segments_on_remote.any?(&:marked_for_deletion?) ).to be(false)
+
+    # package files for job_a should be gone
+    expect( objects_on_remote_for(job_a) ).to be_empty
+    expect( segments_on_remote_for(job_a) ).to be_empty
+  end
+
+  private
+
+  def config
+    config = BackupSpec::LIVE['storage']['cloudfiles']
+    @config ||= {
+      :username           => config['username'],
+      :api_key            => config['api_key'],
+      :auth_url           => config['auth_url'],
+      :region             => config['region'],
+      :servicenet         => config['servicenet'],
+      :container          => config['container'],
+      :segments_container => config['segments_container'],
+      :segment_size       => 0,
+      :max_retries        => 3,
+      :retry_waitsec      => 5
+    }
+  end
+
+  def cloud_io
+    @cloud_io ||= CloudIO::CloudFiles.new(config)
+  end
+
+  def segments_cloud_io
+    @segments_cloud_io ||= CloudIO::CloudFiles.new(
+      config.merge(:container => config[:segments_container])
+    )
+  end
+
+  def files_sent_for(job)
+    job.model.package.filenames.map {|name|
+      File.join(remote_path_for(job), name)
+    }.sort
+  end
+
+  def remote_path_for(job)
+    path = BackupSpec::LIVE['storage']['cloudfiles']['path']
+    package = job.model.package
+    File.join(path, package.trigger, package.time)
+  end
+
+  # objects_on_remote_for(job).map(&:name) should match #files_sent_for(job).
+  # If the files do not exist, or were removed by cycling, this will return [].
+  def objects_on_remote_for(job)
+    cloud_io.objects(remote_path_for(job)).sort_by(&:name)
+  end
+
+  def segments_on_remote_for(job)
+    segments_cloud_io.objects(remote_path_for(job))
+  end
+
+  def clean_remote
+    path = BackupSpec::LIVE['storage']['cloudfiles']['path']
+    objects = cloud_io.objects(path)
+    unless objects.empty?
+      slo_objects, objects = objects.partition(&:slo?)
+      cloud_io.delete_slo(slo_objects)
+      cloud_io.delete(objects)
+    end
+
+    # in case segments are uploaded, but the manifest isn't
+    objects = segments_cloud_io.objects(path)
+    segments_cloud_io.delete(objects) unless objects.empty?
   end
 
 end
