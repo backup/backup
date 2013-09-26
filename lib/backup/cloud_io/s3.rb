7af2dc6501878eeac28ddfe9124a7864dc1a1f1f
Joshua C. Forest <jforest@foresj.net>
2013-09-26 17:18:57 -0400

    Need to add use_iam_profile to the cloud_io constructor

--- lib/backup/cloud_io/s3.rb
+++ lib/backup/cloud_io/s3.rb
@@ -13,14 +13,15 @@ module Backup
       MAX_FILE_SIZE       = 1024**3 * 5   # 5 GiB
       MAX_MULTIPART_SIZE  = 1024**4 * 5   # 5 TiB
 
-      attr_reader :access_key_id, :secret_access_key, :region, :bucket,
-                  :chunk_size, :encryption, :storage_class
+      attr_reader :access_key_id, :secret_access_key, :use_iam_profile, :region,
+                  :bucket, :chunk_size, :encryption, :storage_class
 
       def initialize(options = {})
         super
 
         @access_key_id      = options[:access_key_id]
         @secret_access_key  = options[:secret_access_key]
+        @use_iam_profile    = options[:use_iam_profile]
         @region             = options[:region]
         @bucket             = options[:bucket]
         @chunk_size         = options[:chunk_size]
--- lib/backup/version.rb
+++ lib/backup/version.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 
 module Backup
-  VERSION = '3.7.1'
+  VERSION = '3.7.2'
 end
