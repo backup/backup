40a731de23b5138d08f91024e8661f70663a0d24
Wei <jrweizhang@gmail.com>
2013-10-15 16:15:24 +0800

    Allow Backup to sync remote redis server.

--- lib/backup/database/redis.rb
+++ lib/backup/database/redis.rb
@@ -25,7 +25,7 @@ module Backup
       attr_accessor :password
 
       ##
-      # Connectivity options for the +invoke_save+ option.
+      # Connectivity options for the +invoke_save+ and +sync_remote+ options.
       attr_accessor :host, :port, :socket
 
       ##
@@ -33,6 +33,11 @@ module Backup
       # the `redis-cli` utility to persist the most recent data before
       # copying the dump file specified by +path+ and +name+.
       attr_accessor :invoke_save
+
+      ##
+      # Determines whether Backup should sync to remote server (via `--rdb` option) through
+      # the `redis-cli` utility to dump remote redis's data before copying.
+      attr_accessor :sync_remote
 
       ##
       # Additional "redis-cli" options
@@ -52,10 +57,14 @@ module Backup
       #   <trigger>/databases/Redis[-<database_id>].rdb[.gz]
       #
       # If +invoke_save+ is true, `redis-cli SAVE` will be invoked.
+      # If +sync_remote+ is true, `redis-cli --rdb` will be invoked. If that is
+      # the case, there is no need to invoke_save.
       def perform!
         super
 
+        sync_remote! if sync_remote
         invoke_save! if invoke_save
+
         copy!
 
         log!(:finished)
@@ -69,6 +78,17 @@ module Backup
           raise Error, <<-EOS
             Could not invoke the Redis SAVE command.
             Command was: #{ redis_save_cmd }
+            Response was: #{ resp }
+          EOS
+        end
+      end
+
+      def sync_remote!
+        resp = run(redis_rdb_cmd)
+        unless resp =~ /Transfer finished with success\.$/
+          raise Error, <<-EOS
+            Could not sync to remote Redis.
+            Command was: #{ redis_rdb_cmd }
             Response was: #{ resp }
           EOS
         end
@@ -96,6 +116,12 @@ module Backup
       def redis_save_cmd
         "#{ utility('redis-cli') } #{ password_option } " +
         "#{ connectivity_options } #{ user_options } SAVE"
+      end
+
+      def redis_rdb_cmd
+        "#{ utility('redis-cli') }  #{ password_option } " +
+        "#{ connectivity_options } #{ user_options } " +
+        "--rdb #{ File.join(path, name) }"
       end
 
       def password_option
