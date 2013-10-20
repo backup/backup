7212f273c99502e12baa43d7bd315e9dfcd4a312
Brian D. Burns <iosctr@gmail.com>
2013-10-20 17:53:36 -0400

    update specs

--- spec/storage/sftp_spec.rb
+++ spec/storage/sftp_spec.rb
@@ -18,6 +18,7 @@ describe Storage::SFTP do
       expect( storage.keep        ).to be_nil
       expect( storage.username    ).to be_nil
       expect( storage.password    ).to be_nil
+      expect( storage.ssh_options ).to eq({})
       expect( storage.ip          ).to be_nil
       expect( storage.port        ).to be 22
       expect( storage.path        ).to eq 'backups'
@@ -26,17 +27,19 @@ describe Storage::SFTP do
     it 'configures the storage' do
       storage = Storage::SFTP.new(model, :my_id) do |sftp|
         sftp.keep = 2
-        sftp.username = 'my_username'
-        sftp.password = 'my_password'
-        sftp.ip       = 'my_host'
-        sftp.port     = 123
-        sftp.path     = 'my/path'
+        sftp.username     = 'my_username'
+        sftp.password     = 'my_password'
+        sftp.ssh_options  = { :keys => ['my/key'] }
+        sftp.ip           = 'my_host'
+        sftp.port         = 123
+        sftp.path         = 'my/path'
       end
 
       expect( storage.storage_id  ).to eq 'my_id'
       expect( storage.keep        ).to be 2
       expect( storage.username    ).to eq 'my_username'
       expect( storage.password    ).to eq 'my_password'
+      expect( storage.ssh_options ).to eq :keys => ['my/key']
       expect( storage.ip          ).to eq 'my_host'
       expect( storage.port        ).to be 123
       expect( storage.path        ).to eq 'my/path'
@@ -65,11 +68,13 @@ describe Storage::SFTP do
       storage.ip = '123.45.678.90'
       storage.username = 'my_user'
       storage.password = 'my_pass'
+      storage.ssh_options = { :keys => ['my/key'] }
     end
 
     it 'yields a connection to the remote server' do
       Net::SFTP.expects(:start).with(
-        '123.45.678.90', 'my_user', :password => 'my_pass', :port => 22
+        '123.45.678.90', 'my_user', :password => 'my_pass', :port => 22,
+        :keys => ['my/key']
       ).yields(connection)
 
       storage.send(:connection) do |sftp|
