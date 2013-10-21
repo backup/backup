bb9840a3eb4849c7433252ba16d7257dab07a863
Brian D. Burns <iosctr@gmail.com>
2013-10-20 20:07:13 -0400

    fix syntax error

--- lib/backup/cloud_io/s3.rb
+++ lib/backup/cloud_io/s3.rb
@@ -127,7 +127,7 @@ module Backup
           else
             opts.merge!(
               :aws_access_key_id      => access_key_id,
-              :aws_secret_access_key  => secret_access_key,
+              :aws_secret_access_key  => secret_access_key
             )
           end
           conn = Fog::Storage.new(opts)
