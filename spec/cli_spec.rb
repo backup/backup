82fc0db15ee5d0b8129c0f5815a27bf8f4ed5ea3
bastengao <bastengao@gmail.com>
2016-02-04 14:50:11 +0800

    Add QiniuStorage for cli

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -557,7 +557,7 @@ describe 'Backup::CLI' do
     it 'should include the correct option values' do
       options = <<-EOS.lines.to_a.map(&:strip).map {|l| l.partition(' ') }
         databases (mongodb, mysql, openldap, postgresql, redis, riak)
-        storages (cloud_files, dropbox, ftp, local, rsync, s3, scp, sftp)
+        storages (cloud_files, dropbox, ftp, local, qiniu, rsync, s3, scp, sftp)
         syncers (cloud_files, rsync_local, rsync_pull, rsync_push, s3)
         encryptor (gpg, openssl)
         compressor (bzip2, custom, gzip)
