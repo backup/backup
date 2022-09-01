8d188d7070b0f93f2524d7f4af3fd1768861ed8c
Thomas Stratmann <thomas.stratmann@9elements.com>
2022-09-01 17:59:36 +0200

    [fix me up] use respond_to? instead

--- lib/backup/storage/cycler.rb
+++ lib/backup/storage/cycler.rb
@@ -53,7 +53,7 @@ module Backup
       def yaml_load
         loaded =
           if File.exist?(yaml_file) && !File.zero?(yaml_file)
-            if YAML.name == 'Psych' && Psych::VERSION >= '3.1'
+            if YAML.respond_to?(:safe_load_file)
               YAML.safe_load_file(yaml_file, permitted_classes: [Backup::Package])
             else
               YAML.load_file(yaml_file)
